2f169750d1520a329f821fd00a1bf59b7e9a1eec
Dibo <sh.eldeeb.2010@gmail.com>
2022-03-19 19:03:34 +0000

    delay befor setting the title

--- src/app/feature/lazy.component.ts
+++ src/app/feature/lazy.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'lazy-comp',
@@ -7,7 +9,12 @@ import { Title } from '@angular/platform-browser';
 })
 export class LazyComponent implements OnInit {
   constructor(public titleService: Title) {
-    this.titleService.setTitle('lazy');
+    this.titleService.setTitle('lazy#1');
+
+    // delay 5 seconds
+    of('')
+      .pipe(delay(5000))
+      .subscribe(() => this.titleService.setTitle('lazy#2'));
   }
 
   ngOnInit(): void {}
--- src/app/feature/normal.component.ts
+++ src/app/feature/normal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'normal-comp',
@@ -7,7 +9,12 @@ import { Title } from '@angular/platform-browser';
 })
 export class NormalComponent implements OnInit {
   constructor(public titleService: Title) {
-    this.titleService.setTitle('normal');
+    this.titleService.setTitle('normal#1');
+
+    // delay 5 seconds
+    of('')
+      .pipe(delay(5000))
+      .subscribe(() => this.titleService.setTitle('normal#2'));
   }
 
   ngOnInit(): void {}
